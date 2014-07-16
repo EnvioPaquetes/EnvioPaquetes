0b006ea536c8ec5ca3ce59fa3a7b8a386c56e33a
KarlozV <Karloz.n19@gmail.com>
2014-07-16 14:41:11 -0500

    VistaPaquetes

--- src/Registro/Paquetes.java
+++ src/Registro/Paquetes.java
@@ -11,24 +11,49 @@ package Registro;
  * @author karloz
  */
 public class Paquetes {
-    Integer Cantidad;
-    String Peso;
+    String CedulaRemitente;
+    String NombreRemitente;
+    Integer CantidadPaquetes;
+    String PesoPaquetes;
     String PrecioEnvio;
+    String CodigoPaquete;
+    String Municipio;
+    String Barrio;
+    String Dirreccion;
+    String Conductor;
+    Integer CedulaDestinatario;
+    String NombreDestinatario;
 
-    public Integer getCantidad() {
-        return Cantidad;
+    public String getCedulaRemitente() {
+        return CedulaRemitente;
     }
 
-    public void setCantidad(Integer Cantidad) {
-        this.Cantidad = Cantidad;
+    public void setCedulaRemitente(String CedulaRemitente) {
+        this.CedulaRemitente = CedulaRemitente;
     }
 
-    public String getPeso() {
-        return Peso;
+    public String getNombreRemitente() {
+        return NombreRemitente;
     }
 
-    public void setPeso(String Peso) {
-        this.Peso = Peso;
+    public void setNombreRemitente(String NombreRemitente) {
+        this.NombreRemitente = NombreRemitente;
+    }
+
+    public Integer getCantidadPaquetes() {
+        return CantidadPaquetes;
+    }
+
+    public void setCantidadPaquetes(Integer CantidadPaquetes) {
+        this.CantidadPaquetes = CantidadPaquetes;
+    }
+
+    public String getPesoPaquetes() {
+        return PesoPaquetes;
+    }
+
+    public void setPesoPaquetes(String PesoPaquetes) {
+        this.PesoPaquetes = PesoPaquetes;
     }
 
     public String getPrecioEnvio() {
@@ -38,4 +63,61 @@ public class Paquetes {
     public void setPrecioEnvio(String PrecioEnvio) {
         this.PrecioEnvio = PrecioEnvio;
     }
+
+    public String getCodigoPaquete() {
+        return CodigoPaquete;
+    }
+
+    public void setCodigoPaquete(String CodigoPaquete) {
+        this.CodigoPaquete = CodigoPaquete;
+    }
+
+    public String getMunicipio() {
+        return Municipio;
+    }
+
+    public void setMunicipio(String Municipio) {
+        this.Municipio = Municipio;
+    }
+
+    public String getBarrio() {
+        return Barrio;
+    }
+
+    public void setBarrio(String Barrio) {
+        this.Barrio = Barrio;
+    }
+
+    public String getDirreccion() {
+        return Dirreccion;
+    }
+
+    public void setDirreccion(String Dirreccion) {
+        this.Dirreccion = Dirreccion;
+    }
+
+    public String getConductor() {
+        return Conductor;
+    }
+
+    public void setConductor(String Conductor) {
+        this.Conductor = Conductor;
+    }
+
+    public Integer getCedulaDestinatario() {
+        return CedulaDestinatario;
+    }
+
+    public void setCedulaDestinatario(Integer CedulaDestinatario) {
+        this.CedulaDestinatario = CedulaDestinatario;
+    }
+
+    public String getNombreDestinatario() {
+        return NombreDestinatario;
+    }
+
+    public void setNombreDestinatario(String NombreDestinatario) {
+        this.NombreDestinatario = NombreDestinatario;
+    }
+
 }
